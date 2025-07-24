de1799b0bd16c37c893ab1474a7f63f202c137bd
Your Name <hedermd6@gmail.com>
2025-07-24 16:13:09 -0300

    feat: initial configurations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  const HTTP_PORT = process.env.HTTP_PORT || 3000;
+  const APP_HOSTNAME = process.env.APP_HOSTNAME || 'localhost';
+  const ssl = process.env.SSL === 'true';
+  await app.listen(HTTP_PORT, () => {
+    const address =
+      'http' + (ssl ? 's' : '') + '://' + APP_HOSTNAME + ':' + HTTP_PORT + '/';
+    Logger.log('Listening at ' + address);
+  });
 }
+
 bootstrap();
